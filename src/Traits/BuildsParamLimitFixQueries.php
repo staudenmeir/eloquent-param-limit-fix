2f21613d5cd9a18c1dfe235fbfd5ebc994518f62
Chris Lloyd <chrislloyd403@gmail.com>
2024-05-26 10:00:12 +0100

    Add limit for PostgreSQL

--- src/Traits/BuildsParamLimitFixQueries.php
+++ src/Traits/BuildsParamLimitFixQueries.php
@@ -3,6 +3,7 @@
 namespace Staudenmeir\EloquentParamLimitFix\Traits;
 
 use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\PostgresConnection;
 use Illuminate\Database\SQLiteConnection;
 use Illuminate\Database\SqlServerConnection;
 
@@ -21,6 +22,7 @@ trait BuildsParamLimitFixQueries
         MySqlConnection::class => 65000,
         SQLiteConnection::class => 900,
         SqlServerConnection::class => 2000,
+        PostgresConnection::class => 65000,
     ];
 
     /**
